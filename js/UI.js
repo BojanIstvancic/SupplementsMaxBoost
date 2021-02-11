5614b8a65d0d7d8499a3b3c6e3979155b916e175
BojanIstvancic <bojan.istvancic@gmail.com>
2021-02-11 14:15:54 +0100

    Primary color update, carousel added

--- js/UI.js
+++ js/UI.js
@@ -1,6 +1,40 @@
 class UI {
+  /* Toggle Nav Bar Function */
   toggleNavBar() {
     const navBar = document.querySelector('#navBar');
     navBar.classList.toggle("toggleNavBar");
   }
+
+  /* Reseset classes of elements in Carousel */
+  resetCarouselClasses() {
+    items.forEach(item => item.classList.remove('active'));
+    controls.forEach(control => control.classList.remove('active'));
+  }
+
+  /* Carousel Controlls */
+  carouselControll() {
+    ui.resetCarouselClasses();
+    clearInterval(intervalF);
+
+    const control = this,
+      dataIndex = Number(control.dataset.index);
+
+    control.classList.add('active');
+    items.forEach((item, index) => {
+      if (index === dataIndex) {// Add active class to corresponding slide
+        item.classList.add('active');
+      }
+    });
+    current = dataIndex; // Update current slide
+    intervalF = setInterval(ui.carouselSlidingPictures, interval); // Fire that bad boi back up
+  }
+
+  carouselSlidingPictures() {
+    ui.resetCarouselClasses();
+    if (current === items.length - 1) current = -1; // Check if current slide is last in array
+    current++;
+    controls[current].classList.add('active');
+    items[current].classList.add('active');
+  }
 }
+
--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-const ui = new UI();
+const ui = new UI(),
+  items = document.querySelectorAll('.carouselItem'),
+  controls = document.querySelectorAll('.control'),
+  interval = 2000;
+
+let current = 0;
+
+let intervalF = setInterval(ui.carouselSlidingPictures, interval);
 
 // Create the event listeners
 
@@ -6,9 +13,11 @@ function eventListeners() {
   // Document Ready 
   // document.addEventListener('DOMContentLoaded')
 
-
   const navBarIcons = document.querySelectorAll('#navBar__icon');
-  navBarIcons.forEach( navBarIcon => navBarIcon.addEventListener('click', ui.toggleNavBar));
+  navBarIcons.forEach(navBarIcon => navBarIcon.addEventListener('click', ui.toggleNavBar));
+
+  controls.forEach(control => control.addEventListener('click',  ui.carouselControll))
 }
 
 eventListeners();
+
